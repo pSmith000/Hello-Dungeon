d76020b031f4ff7183e3e474a19d0788da566ee8
Peyton <peytonsemail2000@gmail.com>
2021-08-18 09:57:49 -0500

    Added Test Commit Comment
    Also added on to the game with a sanity addition, a fight, and the end of stage 1.

--- Hello Dungeon/Game.cs
+++ Hello Dungeon/Game.cs
@@ -11,6 +11,7 @@ namespace Hello_Dungeon
             //Initialize variables
             int healthRegen = 50;
             int health = 100;
+            int sanity = 100;
             string name = "Empty";
             string medkit = "";
             string weaponAsk = "";
@@ -34,20 +35,22 @@ namespace Hello_Dungeon
             //Ask for things
             Console.Write("Hello {user}. Please enter your name: ");
             name = Console.ReadLine();
-            Console.WriteLine("Hello " + name + "! Your health is " + health + ".");
+            Console.WriteLine("Hello " + name + "! Your health is " + health + ". And your sanity is " + sanity);
 
             Console.WriteLine("");
             Console.Write("Would you like a weapon? (Y/N): ");
             weaponAsk = Console.ReadLine();
 
+            //Weapon Check
             if (weaponAsk == "Y")
             {
                 weapon = true;
-                Console.WriteLine("Good choice" + name + "... Survival is key here..");
+                Console.WriteLine("Good choice " + name + "... Survival is key here..");
             }
 
             else
             {
+                weapon = false;
                 Console.WriteLine("Hmmm, thats a choice " + name + "... Not a very good one if [REDACTED] comes for you.");
             }
 
@@ -64,11 +67,26 @@ namespace Hello_Dungeon
             else
             {
                 Console.WriteLine("[REDACTED] is coming. You have no escape");
-                if (weapon = true)
+                if (weapon == true)
                 {
-                    Console.WriteLine("Ah you have a weapon. ")
+                    Console.WriteLine("Ah you have a weapon. You may fight. ");
+                    Console.WriteLine("");
+                    Console.WriteLine("You slash and hack through [REDACTED] spewing [REDCATED] blood all over the walls. You take mild injuries.");
+                    health -= 15;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("No weapon either... Only god can save you now");
+                    Console.WriteLine("");
+                    Console.WriteLine("You sit alone in the dark until you feel a soft hand on your shoulder. You look up and see a familiar face.");
+                    Console.WriteLine("[REDACTED] lulls you in and attacks. You take severe injuries. [REDACTED] loves playing with prey.");
+                    health -= 60;
                 }
             }
+            Console.WriteLine("");
+            Console.WriteLine("END OF STAGE 1");
+            Console.WriteLine("Health: " + health);
         }
     }
 }
